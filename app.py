08302a3d209ac5574b8559b4d2e78e2a31919944
Sangyoon Park <datumvitae@gmail.com>
2020-12-29 18:03:25 -0500

    Upgrade 'Venue' and 'Artist' data models

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
-# Models.
+# Data Models
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
@@ -35,8 +35,9 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -46,11 +47,11 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 

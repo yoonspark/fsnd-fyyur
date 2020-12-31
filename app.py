8c61654d8fae4c02b25318256f447570a45ac87c
Sangyoon Park <datumvitae@gmail.com>
2020-12-31 08:15:50 -0500

    Change data type for 'start_time' column

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, compare_type=True)
 
 #----------------------------------------------------------------------------#
 # Data Models
@@ -57,7 +57,7 @@ class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.String(50))
+    start_time = db.Column(db.DateTime)
 
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)

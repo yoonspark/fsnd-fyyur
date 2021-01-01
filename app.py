3777bfac6b5600b581225dc120eda160b027abe8
Sangyoon Park <datumvitae@gmail.com>
2020-12-31 19:02:10 -0500

    Improve datetime formatting

--- app.py
+++ app.py
@@ -109,9 +109,9 @@ class Genre(db.Model):
 def format_datetime(value, format='medium'):
   date = dateutil.parser.parse(value)
   if format == 'full':
-      format="EEEE MMMM, d, y 'at' h:mma"
+      format="EEEE, MMMM d, y 'at' h:mma"
   elif format == 'medium':
-      format="EE MM, dd, y h:mma"
+      format="EE, MM/dd/y, h:mma"
   return babel.dates.format_datetime(date, format)
 
 app.jinja_env.filters['datetime'] = format_datetime

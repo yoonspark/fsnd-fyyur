e535aa1256166d539f24d839d0947c30e70fa153
Sangyoon Park <datumvitae@gmail.com>
2021-01-04 17:02:14 -0500

    Do not use explicit time zone info; this can be deduced from venue location info

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime(timezone=True))
+    start_time = db.Column(db.DateTime)
 
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
